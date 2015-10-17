f3157dcaed20adc1dbe4cea1b4d8b79dc747ec2f
ReMinoer <rmorillon@freneticexperience.com>
2015-10-17 16:07:54 +0200

    Clean interaction scripts

---
+++ Assets/Scripts/Interaction/DoorBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interaction
+{
+    [RequireComponent(typeof(VRActor))]
+    [RequireComponent(typeof(Animator))]
+    public class DoorBehaviour : MonoBehaviour
+    {
+        private bool _isOpen;
+        private Animator _animator;
+        private Renderer _renderer;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _renderer = GetComponentInChildren<Renderer>();
+        }
+
+        protected void OnMVRWandEnter(VRSelection iSelection)
+        {
+            _renderer.material.color = Color.green;
+        }
+
+        protected void OnMVRWandExit(VRSelection iSelection)
+        {
+            _renderer.material.color = Color.white;
+        }
+
+        protected void OnMVRWandButtonPressed(VRSelection iSelection)
+        {
+            if (!_animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle"))
+                return;
+
+            _isOpen = !_isOpen;
+            _animator.SetBool("IsOpen", _isOpen);
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/LightSwitchBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interaction
+{
+    [RequireComponent(typeof(VRActor))]
+    public class LightSwitchBehaviour : MonoBehaviour
+    {
+        public Light Light;
+
+        private const string ButtonName = "Model/Button";
+        private Renderer _buttonRenderer;
+
+        void Awake()
+        {
+            GameObject button = transform.Find(ButtonName).gameObject;
+            _buttonRenderer = button.GetComponent<Renderer>();
+        }
+
+        protected void OnMVRWandEnter(VRSelection iSelection)
+        {
+            _buttonRenderer.material.color = Color.green;
+        }
+
+        protected void OnMVRWandExit(VRSelection iSelection)
+        {
+            _buttonRenderer.material.color = Color.white;
+        }
+
+        protected void OnMVRWandButtonPressed(VRSelection iSelection)
+        {
+            Light.enabled = !Light.enabled;
+        }
+    }
+}
