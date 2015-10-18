200355846736da6c4d4407c150bd8339c8dd8fbc
ClementLabbe <clemlab35@gmail.com>
2015-10-18 14:25:59 +0200

    Add environmental sounds prototype

---
+++ Assets/Scripts/EnvironmentalSound/EnvironmentalSound.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentalSound : MonoBehaviour {
+
+    public AudioSource source;
+    private EnvironmentalSoundTrigger currentSound = null;
+
+    private int fadingTime = 1;
+    private int nbIncrement = 10;
+
+    IEnumerator FadeOut()
+    {
+        float delta = (1 / (float)nbIncrement) * fadingTime;
+        for (int i = 0; i < nbIncrement; i++)
+        {
+            source.volume -= delta;
+            yield return new WaitForSeconds(delta);
+        }
+        yield return new WaitForSeconds(fadingTime);
+    }
+    IEnumerator FadeIn()
+    {
+        float delta = (1 / (float)nbIncrement) * fadingTime;
+        Debug.Log(delta);
+        for (int i = 0; i < nbIncrement; i++)
+        {
+            source.volume += delta;
+            yield return new WaitForSeconds(delta);
+        }
+    }
+
+    public void PlaySound(EnvironmentalSoundTrigger clip)
+    {
+        StartCoroutine(PlaySoundCo(clip));
+    }
+
+    IEnumerator PlaySoundCo(EnvironmentalSoundTrigger clip)
+    {
+        if (clip != currentSound)
+        {
+            if (source.isPlaying)
+            {
+                StartCoroutine(FadeOut());
+                yield return new WaitForSeconds(fadingTime);
+            }
+
+            currentSound = clip;
+
+            source.clip = clip.clip;
+            source.volume = 0;
+            source.Play();
+            StartCoroutine(FadeIn());
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnvironmentalSound/EnvironmentalSoundTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class EnvironmentalSoundTrigger : MonoBehaviour
+{
+    public AudioClip clip;
+    // Note: Ajouter variable pour différencier les salles
+
+    void OnTriggerEnter(Collider col)
+    {
+        EnvironmentalSound player = col.gameObject.GetComponent<EnvironmentalSound>();
+        if (player != null)
+        {
+            player.PlaySound(this);
+        }
+    }
+
+}
