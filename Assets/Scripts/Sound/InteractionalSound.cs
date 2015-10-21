7d44e284dc80f1b9eed3c1c010c9e3c4a06f8974
ClementLabbe <clemlab35@gmail.com>
2015-10-21 12:42:32 +0200

    Clean Environmental Sounds, add interactional Sounds

---
+++ Assets/Scripts/Sound/InteractionalSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VRActor))]
+[RequireComponent(typeof(AudioSource))]
+
+public class InterctionalSound : MonoBehaviour {
+
+    private AudioSource source;
+    public AudioClip clip;
+
+    void Awake()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        source.clip = clip;
+    }
+
+    protected void OnMVRWandButtonPressed(VRSelection iSelection)
+    {
+        source.Play();
+    }
+}
