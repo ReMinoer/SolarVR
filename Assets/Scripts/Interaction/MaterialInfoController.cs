adedc702b756f915a73d5417047117f6d2e65874
ReMinoer <rmorillon@freneticexperience.com>
2015-12-10 17:23:46 +0100

    Material info : first prototype

---
+++ Assets/Scripts/Interaction/MaterialInfoController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MaterialInfoController : MonoBehaviour
+{
+    public MaterialInfoDictionary Dictionary;
+    public MaterialInfoView View;
+    public string WandNodeName = "HandNode";
+
+    private Material _currentMaterial;
+    private GameObject _wand;
+
+    void Start()
+    {
+        _wand = GameObject.Find(WandNodeName);
+    }
+
+	void Update ()
+    {
+        var ray = new Ray(_wand.transform.position, _wand.transform.forward);
+	        
+        RaycastHit hit;
+	    if (Physics.Raycast(ray, out hit))
+	    {
+	        var materialRenderer = hit.collider.gameObject.GetComponent<Renderer>();
+            if (materialRenderer != null)
+            {
+                if (MiddleVR.VRDeviceMgr.IsWandButtonPressed(1))
+                {
+                    MaterialInfo materialInfo = Dictionary[materialRenderer.material];
+                    if (materialInfo != null)
+                    {
+                        View.Show(materialInfo, hit);
+                        _currentMaterial = materialRenderer.material;
+                        return;
+                    }
+                }
+
+                if (_currentMaterial != null && _currentMaterial.mainTexture != materialRenderer.material.mainTexture)
+                {
+                    View.Hide();
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Interaction/MaterialInfoDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class MaterialInfoDictionary : MonoBehaviour
+{
+    public MaterialInfo this[Material material]
+    {
+        get { return MaterialInfos.FirstOrDefault(x => x.Material.mainTexture == material.mainTexture); }
+    }
+
+    public MaterialInfo[] MaterialInfos;
+}
+
+[Serializable]
+public class MaterialInfo
+{
+    public Material Material;
+    public string Name;
+
+    [Multiline]
+    public string Description;
+}
